97f3cac40c18e948b8586a47f95d4b73cb81eec6
idris_babay_cgi <idrisbabay.cgi@gmail.com>
2023-02-14 18:03:14 +0100

    slight modification to accomodate that cropped vids are now placed in label directories

--- frame_generator.py
+++ frame_generator.py
@@ -1,5 +1,6 @@
 import pathlib
 import random
+import json
 import cv2
 import tensorflow as tf
 import numpy as np
@@ -42,12 +43,11 @@ def frames_from_video_file(video_path, n_frames, output_size=(224, 224), frame_s
             result.append(np.zeros_like(result[0]))
     src.release()
     result = np.array(result)[..., [2, 1, 0]]
-
     return result
 
 
 class FrameGenerator:
-    def __init__(self, path: pathlib.Path, videos_dir: pathlib.Path, idx_2_label: dict, training: bool = False, instance_idx: list = [], n_frames: int = 74):
+    def __init__(self, videos_dir: pathlib.Path, idx_2_label: dict, training: bool = False, instance_idx: list = [], n_frames: int = 74):
         '''
         Reads frames from the given (root) path, 
         and returns them coupled with their associated label/class
@@ -58,35 +58,19 @@ class FrameGenerator:
             path: root path where frames are stored into subdirectories
             training: determine whether the training dataset is being generated
         '''
-        self.path = path
         self.videos_dir = videos_dir
         self.idx_2_label = idx_2_label
         self.training = training
         self.instance_idx = instance_idx
         self.n_frames = n_frames
-        self.class_names = sorted(
-            set(p.name for p in self.path.iterdir() if p.is_dir()))
         self.class_ids = dict((name, idx)
-                              for idx, name in enumerate(self.class_names))
-
-    def get_frames_and_class_name_for_each_instance(self):
-        result = list()
-        for idx in self.instance_idx:
-            frames_dir_str = f'instance_{idx}'
-            frames_paths = list(self.path.glob(
-                '*/' + frames_dir_str + '/*.png'))
-            if len(frames_paths) == 0:
-                print(f'No frames have been extracted for instance {idx}')
-                continue
-            label = frames_paths[0].parent.parent.name
-            result.append((frames_paths, label))
-        return result
+                              for idx, name in enumerate(DS_CLASSES))
 
     def get_videos_files_and_class_names(self):
         video_file_paths = []
         classes = []
         for idx in self.instance_idx:
-            video_file_paths.extend(self.videos_dir.glob(f'{idx}.mp4'))
+            video_file_paths.extend(self.videos_dir.glob(f'*/{idx}.mp4'))
             classes.append(self.idx_2_label[idx]['clean_text'])
         return video_file_paths, classes
 
@@ -103,23 +87,29 @@ class FrameGenerator:
             yield video_frames, label
 
 
-# NUM_FRAMES = 74
+NUM_FRAMES = 74
 
-# data_dir_path = pathlib.Path('data')
-# output_dir_path = data_dir_path / 'output'
-# frames_dir_path = output_dir_path / 'frames'
-# videos_dir_path = data_dir_path / 'videos'
-# cropped_videos_dir_path = videos_dir_path / 'cropped'
-# dataset_dir_path = data_dir_path / 'dataset'
-# cleansed_training_data_path = pathlib.Path('cleansed_train_ds.json')
+data_dir_path = pathlib.Path('data')
+output_dir_path = data_dir_path / 'output'
+frames_dir_path = output_dir_path / 'frames'
+videos_dir_path = data_dir_path / 'videos' / 'train'
+cropped_videos_dir_path = videos_dir_path / 'cropped'
+dataset_dir_path = data_dir_path / 'dataset'
+DS_CLASSES_PATH = dataset_dir_path / 'MSASL_classes.json'
+cleansed_training_data_path = pathlib.Path('cleansed_train_ds.json')
 
-# with open(cleansed_training_data_path, 'r') as f_:
-#     training_data = json.load(f_)
-#     f_.close()
+with open(DS_CLASSES_PATH, 'r') as f_:
+    DS_CLASSES = json.load(f_)
+    f_.close()
+
+with open(cleansed_training_data_path, 'r') as f_:
+    training_data = json.load(f_)
+    f_.close()
+
 
 # instance_idx = [45, 46, 59, 121, 122, 178, 179, 180, 181, 184, 199, 200, 275, 276, 277, 278, 279, 292, 316, 328, 329, 330, 334, 340, 367,
 #                 396, 421, 470, 477, 596, 633, 634, 635, 636, 637, 644, 686, 700, 775, 885, 995, 1084, 1149, 1241, 1310, 1421, 1424, 1661, 1967, 2037]
-# fg = FrameGenerator(frames_dir_path, cropped_videos_dir_path,
+# fg = FrameGenerator(cropped_videos_dir_path,
 #                     training_data, True, instance_idx)
 # i = 0
 # for video_frames, label in fg():
--- optical_flow_generator.py
+++ optical_flow_generator.py
@@ -8,9 +8,9 @@ import cv2
 
 
 class OpticalFlowStream:
-    def __init__(self, path: pathlib.Path, videos_dir: pathlib.Path, idx_2_label: dict, training: bool = False, instance_idx: list = [], n_frames: int = 74):
+    def __init__(self, videos_dir: pathlib.Path, idx_2_label: dict, training: bool = False, instance_idx: list = [], n_frames: int = 74):
         self.frame_generator = FrameGenerator(
-            path, videos_dir, idx_2_label, training, instance_idx, n_frames)
+            videos_dir, idx_2_label, training, instance_idx, n_frames)
 
     def __call__(self):
         for video_frames, label in self.frame_generator():
@@ -30,7 +30,7 @@ class OpticalFlowStream:
 data_dir_path = pathlib.Path('data')
 output_dir_path = data_dir_path / 'output'
 frames_dir_path = output_dir_path / 'frames'
-videos_dir_path = data_dir_path / 'videos'
+videos_dir_path = data_dir_path / 'videos' / 'train'
 cropped_videos_dir_path = videos_dir_path / 'cropped'
 dataset_dir_path = data_dir_path / 'dataset'
 cleansed_training_data_path = pathlib.Path('cleansed_train_ds.json')
@@ -41,7 +41,7 @@ with open(cleansed_training_data_path, 'r') as f_:
 
 instance_idx = [45, 46, 59, 121, 122, 178, 179, 180, 181, 184, 199, 200, 275, 276, 277, 278, 279, 292, 316, 328, 329, 330, 334, 340, 367,
                 396, 421, 470, 477, 596, 633, 634, 635, 636, 637, 644, 686, 700, 775, 885, 995, 1084, 1149, 1241, 1310, 1421, 1424, 1661, 1967, 2037]
-# fg = OpticalFlowStream(frames_dir_path, cropped_videos_dir_path,
+# fg = OpticalFlowStream(cropped_videos_dir_path,
 #                        training_data, True, instance_idx)
 # i = 0
 # for flow_stream, label in fg():
