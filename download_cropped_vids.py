e9655fdf227054af20ce633eebd5c842a42772fb
idris_babay_cgi <idrisbabay.cgi@gmail.com>
2023-02-22 09:43:40 +0100

    accomodate constants and utility functions refactoring, add types to vars and functions params

--- download_cropped_vids.py
+++ download_cropped_vids.py
@@ -6,120 +6,53 @@ This file allows the downloading and cropping of the dataset videos to their cor
 '''
 
 # Imports
-
-import os
-import json
+import pathlib
 import re
 import pafy
 import ffmpeg
-import numpy as np
-import pathlib
+# import numpy as np
+# import collections
+
+import constants
+from utilities import read_dict_from_JSON, save_dict_as_JSON_file
 
 # Constants
-VID_SIZE_THRESHOLD = 10000000
-
-DATA_DIR = 'data'
-
-ASSETS_DIR = os.path.join(DATA_DIR, 'assets')
-
-TRAIN_IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH = os.path.join(
-    ASSETS_DIR, 'train_idx_2_init_dwnld_idx_w_same_url.json')
-TRAIN_INSTANCE_2_TRIMMED_PATH = os.path.join(
-    ASSETS_DIR, 'train_instance_2_trimmed.json')
-TRAIN_INSTANCE_2_CROPPED_PATH = os.path.join(
-    ASSETS_DIR, 'train_instance_2_cropped.json')
-
-VAL_IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH = os.path.join(
-    ASSETS_DIR, 'val_idx_2_init_dwnld_idx_w_same_url.json')
-VAL_INSTANCE_2_TRIMMED_PATH = os.path.join(
-    ASSETS_DIR, 'val_instance_2_trimmed.json')
-VAL_INSTANCE_2_CROPPED_PATH = os.path.join(
-    ASSETS_DIR, 'val_instance_2_cropped.json')
-
-TEST_IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH = os.path.join(
-    ASSETS_DIR, 'test_idx_2_init_dwnld_idx_w_same_url.json')
-TEST_INSTANCE_2_TRIMMED_PATH = os.path.join(
-    ASSETS_DIR, 'test_instance_2_trimmed.json')
-TEST_INSTANCE_2_CROPPED_PATH = os.path.join(
-    ASSETS_DIR, 'test_instance_2_cropped.json')
-
-DATASET_DIR = os.path.join(ASSETS_DIR, 'dataset')
-
-TRAIN_DATA_PATH = os.path.join(DATASET_DIR, 'MSASL_train.json')
-VAL_DATA_PATH = os.path.join(DATASET_DIR, 'MSASL_val.json')
-TEST_DATA_PATH = os.path.join(DATASET_DIR, 'MSASL_test.json')
-
-VIDEOS_DIR = os.path.join(DATA_DIR, 'videos')
-
-TRAIN_VID_DIR = os.path.join(VIDEOS_DIR, 'train')
-DOWNLOADED_TRAIN_DIR = os.path.join(TRAIN_VID_DIR, 'downloaded')
-TRIMMED_TRAIN_DIR = os.path.join(TRAIN_VID_DIR, 'trimmed')
-CROPPED_TRAIN_DIR = os.path.join(TRAIN_VID_DIR, 'cropped')
-
-VAL_VID_DIR = os.path.join(VIDEOS_DIR, 'val')
-DOWNLOADED_VAL_DIR = os.path.join(VAL_VID_DIR, 'downloaded')
-TRIMMED_VAL_DIR = os.path.join(VAL_VID_DIR, 'trimmed')
-CROPPED_VAL_DIR = os.path.join(VAL_VID_DIR, 'cropped')
-
-TEST_VID_DIR = os.path.join(VIDEOS_DIR, 'test')
-DOWNLOADED_TEST_DIR = os.path.join(TEST_VID_DIR, 'downloaded')
-TRIMMED_TEST_DIR = os.path.join(TEST_VID_DIR, 'trimmed')
-CROPPED_TEST_DIR = os.path.join(TEST_VID_DIR, 'cropped')
+VID_SIZE_THRESHOLD = 25000000
 
 DATA_ATTR = ['org_text', 'clean_text', 'start_time', 'signer_id', 'signer', 'start',
              'end', 'file', 'label', 'height', 'fps', 'end_time', 'url', 'text', 'box', 'width']
 
-# Utility functions
-
-
-def save_dict_as_JSON_file(path_, data_):
-    with open(path_, 'w') as f:
-        json.dump(data_, f, indent=2)
-        print('Saved dictionary to the JSON file.')
-        f.close()
-
-
-def read_dict_from_JSON(path_):
-    with open(path_, 'r') as f:
-        result = json.load(f)
-        f.close()
-    return result
-
-
-# /Utility functions
-
 # Change these constants to TRAIN/VAL/TEST
-DOWNLOADED_DIR = DOWNLOADED_VAL_DIR
-TRIMMED_DIR = TRIMMED_VAL_DIR
-CROPPED_DIR = CROPPED_VAL_DIR
-IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH = VAL_IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH
-INSTANCE_2_TRIMMED_PATH = VAL_INSTANCE_2_TRIMMED_PATH
-INSTANCE_2_CROPPED_PATH = VAL_INSTANCE_2_CROPPED_PATH
-DATA_PATH = VAL_DATA_PATH
+DATA = constants.VAL_DATA
+DOWNLOADED_DIR = constants.VAL_VIDS_DWNLD_DIR
+TRIMMED_DIR = constants.VAL_VIDS_TRIMMED_DIR
+CROPPED_DIR = constants.VAL_VIDS_CROPPED_DIR
+IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH = constants.VAL_IDX_2_INIT_DWNLD_PATH
+INSTANCE_2_TRIMMED_PATH = constants.VAL_INSTANCE_2_TRIMMED_PATH
+INSTANCE_2_CROPPED_PATH = constants.VAL_INSTANCE_2_CROPPED_PATH
 
 
 # Create directories and files if not existant
-if not os.path.exists(DOWNLOADED_DIR):
-    os.makedirs(DOWNLOADED_DIR)
-if not os.path.exists(TRIMMED_DIR):
-    os.makedirs(TRIMMED_DIR)
-if not os.path.exists(CROPPED_DIR):
-    os.makedirs(CROPPED_DIR)
+if not DOWNLOADED_DIR.exists():
+    DOWNLOADED_DIR.mkdir(parents=True)
+if not TRIMMED_DIR.exists():
+    TRIMMED_DIR.mkdir(parents=True)
+if not CROPPED_DIR.exists():
+    CROPPED_DIR.mkdir(parents=True)
 
 
 # Create JSON files if not already done that will serve as permanent storage of dicts over the potentially multiple runs
 # and write an empty dict to them to avoid errors when attempting to parse their content
 placeholder_dict = dict()
-if not os.path.exists(IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH):
+if not IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH.exists():
     save_dict_as_JSON_file(
         IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH, placeholder_dict)
-if not os.path.exists(INSTANCE_2_TRIMMED_PATH):
+if not INSTANCE_2_TRIMMED_PATH.exists():
     save_dict_as_JSON_file(INSTANCE_2_TRIMMED_PATH, placeholder_dict)
-if not os.path.exists(INSTANCE_2_CROPPED_PATH):
+if not INSTANCE_2_CROPPED_PATH.exists():
     save_dict_as_JSON_file(INSTANCE_2_CROPPED_PATH, placeholder_dict)
 
 # read JSON files as python dicts
-DATA = read_dict_from_JSON(DATA_PATH)
 IDX_2_INIT_DWNLD_IDX_W_SAME_URL = read_dict_from_JSON(
     IDX_2_INIT_DWNLD_IDX_W_SAME_URL_PATH)
 INSTANCE_2_TRIMMED = read_dict_from_JSON(INSTANCE_2_TRIMMED_PATH)
@@ -138,20 +71,20 @@ def mycb(total, recvd, ratio, rate, eta):
           'ratio:', ratio, 'rate:', rate, 'eta:', eta)
 
 
-def remove_http_or_https_from_url(url):
+def remove_http_or_https_from_url(url: str):
     return re.sub(r'(http|https)://', '', url)
 
 
-def clean_up_files_in_dir(DIR_):
-    file_path_list = os.listdir(DIR_)
-    for path_ in file_path_list:
-        os.remove(os.path.join(DIR_, path_))
+def clean_up_files_in_dir(DIR_: pathlib.Path):
+    for path_ in DIR_.iterdir():
+        if path_.is_file():
+            path_.unlink()
 
 
-def trim_in_file_and_save_to_out_file(in_file, out_file, start, end):
+def trim_in_file_and_save_to_out_file(in_file: pathlib.Path, out_file: pathlib.Path, start: int, end: int):
     # to avoid overriding confirmation prompt of the ffmpeg-cli
-    if os.path.exists(out_file):
-        os.remove(out_file)
+    if out_file.is_file():
+        out_file.unlink()
 
     # probe function delivers useful information about the signal file
     probe_result = ffmpeg.probe(in_file)
@@ -173,7 +106,7 @@ def trim_in_file_and_save_to_out_file(in_file, out_file, start, end):
     output.run()
 
 
-def remove_normalization_coordinates(x_norm, y_norm, width, height):
+def remove_normalization_coordinates(x_norm: float, y_norm: float, width: float, height: float):
     '''
     Since the coordinates are normalized to the width/height of the video, we simply multiply by them appropriately
     '''
@@ -182,21 +115,22 @@ def remove_normalization_coordinates(x_norm, y_norm, width, height):
     return x, y
 
 
-def crop_video(in_vid, out_vid, x, y, width, height):
+def crop_video(in_vid: pathlib.Path, out_vid: pathlib.Path, x: float, y: float, width: float, height: float):
     '''
     (x, y) correspond to the coordinates of the top-left corner to start cropping from
     width determines how far to travel horizontally from the vertical line defined by the x parameter
     height determines how far to travel vertically from the horizontal line defined by the y parameter
     '''
-    input_stream = ffmpeg.input(in_vid)
-    pts = 'PTS-STARTPTS'
-    cropped = ffmpeg.crop(input_stream, x, y, width, height).setpts(pts)
+    if not out_vid.exists():
+        input_stream = ffmpeg.input(in_vid)
+        pts = 'PTS-STARTPTS'
+        cropped = ffmpeg.crop(input_stream, x, y, width, height).setpts(pts)
 
-    output = ffmpeg.output(cropped, out_vid, format='mp4')
-    output.run()
+        output = ffmpeg.output(cropped, out_vid, format='mp4')
+        output.run()
 
 
-def update_instances_dwnld_state_w_same_vid_url(j, instance):
+def update_instances_dwnld_state_w_same_vid_url(j: int, instance):
     instance_url = instance['url']
     for i, instance_tmp in enumerate(DATA):
         if instance_tmp['url'] == instance_url:
@@ -205,20 +139,20 @@ def update_instances_dwnld_state_w_same_vid_url(j, instance):
     IDX_2_INIT_DWNLD_IDX_W_SAME_URL[str(j)] = str(-2)  # means downloaded
 
 
-def download_unique_vid_of_instance_and_update_map(i, instance):
+def download_unique_vid_of_instance_and_update_map(i: int, instance):
     vid_url = remove_http_or_https_from_url(instance['url'])
     print(i)
     dwnlnd_idx = IDX_2_INIT_DWNLD_IDX_W_SAME_URL.get(str(i), -1)
     # if it has not been downloaded before
     if dwnlnd_idx == -1:
         try:
+            print(vid_url)
             YT_vid = pafy.new(vid_url)
             s = YT_vid.getbest(preftype='mp4')
             s_filesize = s.get_filesize()
             print('stream file size:', s_filesize)
             if s_filesize < VID_SIZE_THRESHOLD:  # because of pafy"s slow download rate, only download videos of size less than a certain threshold, remove this later on
-                downloaded_vid_path = os.path.join(
-                    DOWNLOADED_DIR, str(i) + '.mp4')
+                downloaded_vid_path = DOWNLOADED_DIR / (str(i) + '.mp4')
                 s.download(filepath=downloaded_vid_path, quiet=True,
                            callback=mycb)  # starts download
                 print(downloaded_vid_path)
@@ -259,7 +193,7 @@ def download_unique_vids_of_indices_instances_and_return_url2path_map(indices):
     return vids_w_size_over_threshold
 
 
-def download_unique_vids_of_instances_in_range_and_return_url2path_map(start, end):
+def download_unique_vids_of_instances_in_range_and_return_url2path_map(start: int, end: int):
     '''
     The same functionality as download_unique_vids_of_indices_instances_and_return_url2path_map
     But this one takes in start and end as a range for indices instead of an explicit list
@@ -276,7 +210,7 @@ def download_unique_vids_of_instances_in_range_and_return_url2path_map(start, en
     return vids_w_size_over_threshold
 
 
-def trim_instance_vid_and_update_map(i, instance):
+def trim_instance_vid_and_update_map(i: int, instance):
 
     vid_url = remove_http_or_https_from_url(instance['url'])
     idx_dwnld_status = IDX_2_INIT_DWNLD_IDX_W_SAME_URL.get(str(i), str(-1))
@@ -289,19 +223,18 @@ def trim_instance_vid_and_update_map(i, instance):
         dwnld_init_idx = str(i)
     else:
         dwnld_init_idx = idx_dwnld_status # refer to the instance that initiated it instead as in the construction of IDX_2_INIT_DWNLD_IDX_W_SAME_URL
-    vid_path = os.path.join(DOWNLOADED_DIR, f'{dwnld_init_idx}.mp4')
+    vid_path = DOWNLOADED_DIR / f'{dwnld_init_idx}.mp4'
     if INSTANCE_2_TRIMMED.get(str(i), None) is None:
         print(i, ':', vid_url, '=>', vid_path)
-        class_path = os.path.join(
-            TRIMMED_DIR, instance['clean_text'].replace(' ', '_'))
-        if not os.path.exists(class_path):
-            os.mkdir(class_path)
+        class_path: pathlib.Path = TRIMMED_DIR / str(instance['clean_text'].replace(' ', '_'))
+        if not class_path.exists():
+            class_path.mkdir()
         clip_start = instance['start_time']
         clip_end = instance['end_time']
-        trimmed_vid_path = os.path.join(class_path, f'{i}.mp4')
+        trimmed_vid_path = class_path / f'{i}.mp4'
         trim_in_file_and_save_to_out_file(
             vid_path, trimmed_vid_path, clip_start, clip_end)
-        INSTANCE_2_TRIMMED[str(i)] = str(trimmed_vid_path)
+        INSTANCE_2_TRIMMED[str(i)] = trimmed_vid_path.resolve().__str__()
 
     # if it exists in instance2trimmed, then it's already been trimmed before therefore skip it
     else:
@@ -324,7 +257,7 @@ def trim_vids_for_indices_instances_and_return_updated_map(indices):
     pass
 
 
-def trim_vids_for_instances_in_range_and_return_updated_map(start, end):
+def trim_vids_for_instances_in_range_and_return_updated_map(start: int, end: int):
     '''
     The same functionality as trim_vids_for_indices_instances_and_return_updated_map
     But this one takes in start and end as a range for indices instead of an explicit list
@@ -336,7 +269,7 @@ def trim_vids_for_instances_in_range_and_return_updated_map(start, end):
     pass
 
 
-def crop_vid_for_instance_and_update_map(i, instance):
+def crop_vid_for_instance_and_update_map(i: int, instance):
     '''
     Crops each instance in data provided by indices according to the instance's relevant ROI attributes
     data: list of instances that have the same dataset attributes
@@ -356,18 +289,18 @@ def crop_vid_for_instance_and_update_map(i, instance):
         print(
             i, ': [This instance\'s video is private or inaccessible, skipping...]')
         return
+    in_vid_path = pathlib.Path(in_vid_path)
 
     if INSTANCE_2_CROPPED.get(str(i), None) is None:
-        class_path = os.path.join(
-            CROPPED_DIR, instance['clean_text'].replace(' ', '_'))
-        if not os.path.exists(class_path):
-            os.mkdir(class_path)
-        out_vid_path = os.path.join(class_path, str(i) + '.mp4')
+        class_path = CROPPED_DIR / str(instance['clean_text'].replace(' ', '_'))
+        if not class_path.exists():
+            class_path.mkdir(parents=True)
+        out_vid_path: pathlib.Path = class_path / str(i) + '.mp4'
         print(i, ', cropping from source =>', in_vid_path)
         crop_video(in_vid_path, out_vid_path,
                    x_topleft, y_topleft, width, height)
         # update map/result
-        INSTANCE_2_CROPPED[str(i)] = str(out_vid_path)
+        INSTANCE_2_CROPPED[str(i)] = out_vid_path.resolve().__str__()
     else:
         print(i, ': [Already cropped, skipping...]')
 
@@ -379,7 +312,7 @@ def crop_vids_for_indices_instances_and_return_updated_map(indices):
     pass
 
 
-def crop_vids_for_instances_in_range_and_return_updated_map(start, end):
+def crop_vids_for_instances_in_range_and_return_updated_map(start: int, end: int):
     '''
     The same functionality as crop_vids_for_indices_instances_and_return_updated_map
     But this one takes in start and end as a range for indices instead of an explicit list
@@ -424,7 +357,7 @@ def dwnld_trim_crop_pipeline_indices(indices=[]):
     return vids_w_size_over_threshold
 
 
-def dwnld_trim_crop_pipeline_range(range_start=0, range_end=0):
+def dwnld_trim_crop_pipeline_range(range_start: int=0, range_end: int=0):
     '''
     Downloading, trimming, and then cropping will be done in batches due to the limitations in storage and computation power of the local machine.
     The batches are defined/delimited by the range_start and range_end parameters, which define a range of indices
@@ -458,7 +391,3 @@ def dwnld_trim_crop_pipeline_range(range_start=0, range_end=0):
 
 
 # /Functions
-
-# dummy_indices=[0, 1, 3, 8, 56, 60, 72, 110, 151, 499, 858, 1049]
-# dwnld_trim_crop_pipeline_indices(training_data, indices=dummy_indices)
-# dwnld_trim_crop_pipeline_range(training_data, 0, 150)
