542481888b73705dd8e13042b3aa67d949101cb1
idris_babay_cgi <idrisbabay.cgi@gmail.com>
2023-03-06 17:00:25 +0100

    save_dict_as_JSON_file creates file to write to if non existent, add function to create dummy JSON file if non existent

--- src/utilities.py
+++ src/utilities.py
@@ -2,9 +2,11 @@ import json
 import pathlib
 
 def save_dict_as_JSON_file(path_: pathlib.Path, data_: dict):
+    if not path_.exists():
+        path_.touch()
     with open(path_, 'w') as f:
         json.dump(data_, f, indent=2)
-        print('Saved dictionary to the JSON file.')
+        print(f'Saved dictionary to the {path_.resolve()} JSON file.')
         f.close()
 
 
@@ -13,3 +15,9 @@ def read_dict_from_JSON(path_: pathlib.Path):
         result: dict = json.load(f)
         f.close()
     return result
+
+def create_dummy_JSON_file(path_: pathlib.Path):
+    if not path_.exists():
+        path_.touch()
+    data = dict()
+    save_dict_as_JSON_file(path_, data)
